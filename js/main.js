6a355ee731f35ac36e1d5f72c005de3bbf5458c5
akramrakhim <akramrakhimov4@gmail.com>
2023-12-09 09:36:22 +0500

    Add FAQ section and accordion

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ const elsTabsItem = document.querySelectorAll('.tabs__item');
 const elsTabsPanel = document.querySelectorAll('.tabpanels__item');
 const elsTabLink = document.querySelectorAll('.js-tab-link');
 
+
+const elsAccordionItem = document.querySelectorAll('.accordion__item');
+const elsAccordionItemToggler = document.querySelectorAll('.accordion__item-toggler');
+
 function deactivateTabItem (){
     elsTabsItem.forEach(function (elsTabsItem){
         elsTabsItem.classList.remove('tabs__item--active');
@@ -13,6 +17,14 @@ function deactivateTabPanels (){
         elTabsPanel.classList.remove('tabpanels__item--active');
     });
 }
+
+function closeAccordionItems(){
+    elsAccordionItem.forEach(function (elAccordionItem) {
+        elAccordionItem.classList.remove('accordion__item--open');
+    })
+}
+
+
 
 elsTabLink.forEach(function (elTabLink) {
     elTabLink.addEventListener('click', function (evt) {
@@ -32,5 +44,14 @@ elsTabLink.forEach(function (elTabLink) {
         const elTargetPanel  = document.querySelector(elTabLink.dataset.tabTarget);
         
         elTargetPanel.classList.add('tabpanels__item--active');
+    });
+});
+
+
+elsAccordionItemToggler.forEach(function (elAccordionItemToggler){
+    elAccordionItemToggler.addEventListener('click', function (){
+        closeAccordionItems();
+
+        elAccordionItemToggler.closest('.accordion__item').classList.add('accordion__item--open');
     });
 });
